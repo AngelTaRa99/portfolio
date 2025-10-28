6e1b2cda179c3c8cdcab2124e84a8a5d69ed4d33
Angel T Ramirez <angel.t.r@hotmail.com>
2025-10-28 14:31:11 -0700

    Add style to the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-black">
-      <header className="flex bg-white/45 backdrop-blur-md alingn-center items-center justify-between w-4/5 p-4 px-25 rounded-full shadow-lg mt-5 mb-20 ">
-        <h1 className="text-2xl tracking-widest shadow-2xl">Angel T. Ramirez</h1>
-        <div className="flex flex-row gap-4 text-lg bg-white/45 text-black rounded-full px-4 py-4 backdrop-blur-md">
+    <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-black min-w-100">
+      <header className="sm:flex flex-col sm:flex-row bg-white/45 backdrop-blur-md alingn-center items-center justify-between w-4/5 p-4 px-15 rounded-full shadow-lg mt-5 mb-10 flex-row sticky top-2">
+        <h1 className="text-2xl tracking-widest shadow-2xl mb-2 text-center">Angel T. Ramirez</h1>
+        <div className="flex flex-row gap-2 text-lg bg-gradient-to-r from-slate-900 to-slate-700 text-gray-300 rounded-full px-4 py-4 backdrop-blur-md sm:gap-8">
           <a href="">Home</a>
           <a href="">About</a>
           <a href="">Projects</a>
-          <button className="border rounded-2xl px-8 hover:bg-white hover:text-black transition-all duration-250">Get in touch</button>
+          <button className="hidden md:block items-center border rounded-2xl px-4 hover:bg-white hover:text-black transition-all duration-250">Get in touch</button>
         </div>
       </header>
-      <body>
-        <main className="flex flex-col alingn-center justify-center text-center mt-20">
+      <div className="h-screen">
+        <div className="flex flex-col alingn-center justify-center text-center mt-20 ">
           <Image
             src="/user.jpg"
             alt="Profile Picture"
@@ -25,8 +25,11 @@ export default function Home() {
           <p className="text-lg text-gray-600 dark:text-gray-300 max-w-xl mx-auto">
             Welcome to my personal portfolio. I am a developer passionate about creating innovative and efficient solutions. Explore my projects and feel free to contact me to collaborate.
           </p>
-        </main>
-      </body>
+        </div>
+      </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti sunt autem aliquam labore, impedit aspernatur atque dolores vitae minus repudiandae tempore voluptas hic. Fugit, voluptatibus magnam illo excepturi vero sit!
+        </div>
     </div>
   );
 }
